fe8701434034e449aeb9933109d6950e16cad13e
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-10 02:29:12 -0800

    Dojo Survey

--- dojoSurvey/server.py
+++ dojoSurvey/server.py
@@ -1,23 +1,4 @@
-# from flask import Flask, render_template, request
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/result', methods=['POST'])
-# def result():
-#     name = request.form['name']
-#     location = request.form['location']
-#     language = request.form['language']
-#     comment = request.form['comment']
-#     return render_template('result.html',name=name,location=location,comment=comment)
-#
-# app.run(debug=True)
-
-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -27,11 +8,10 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    print "Submitted Info"
     name = request.form['name']
     location = request.form['location']
     language = request.form['language']
     comment = request.form['comment']
-    return render_template('result.html', name=name, location=location,language=language,comment=comment)
+    return render_template('result.html',name=name,location=location,comment=comment)
 
 app.run(debug=True)
