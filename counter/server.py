48e2327313ee8e98002f2a988f4c819880ab36d5
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-10 14:02:48 -0800

    counter

--- counter/server.py
+++ counter/server.py
@@ -1,24 +1,25 @@
 from flask import Flask,render_template,redirect,request,session
 
 app = Flask(__name__)
-app.secret_key = 'mySecret'
+app.secret_key = "myKey"
+
 @app.route('/')
 def index():
     try:
-        session['count'] += 1
+        session['counter'] += 1
     except:
-        session['count'] = 1
+        session['counter'] = 1
     return render_template('index.html')
 
-@app.route('/add', methods = ['POST'])
-def add():
-    session['count'] += 1
-    return redirect('/')
+@app.route('/ninja',methods = ['POST'])
+def ninja():
+    session['counter'] += 1
+    return redirect ('/')
 
-@app.route('/clear', methods = ['POST'])
-def clear():
-    session['count'] = 0
-    return redirect('/')
+@app.route('/hacker', methods = ['POST'])
+def hacker():
+    session['counter'] = 0
+    return redirect ('/')
 
 
 app.run(debug=True)
