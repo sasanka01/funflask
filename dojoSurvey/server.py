14ae4066f21018c307285eb434c37d53eaad87bd
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-10 02:16:54 -0800

    Dojo Survey

--- dojoSurvey/server.py
+++ dojoSurvey/server.py
@@ -1,3 +1,22 @@
+# from flask import Flask, render_template, request
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+# @app.route('/result', methods=['POST'])
+# def result():
+#     name = request.form['name']
+#     location = request.form['location']
+#     language = request.form['language']
+#     comment = request.form['comment']
+#     return render_template('result.html',name=name,location=location,comment=comment)
+#
+# app.run(debug=True)
+
+
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
