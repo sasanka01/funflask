8e58a17e25f935bf846cad8dd7a46cb6837a77c4
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-11 22:29:51 -0800

    dojo survey with validation

--- surveyValidation/server.py
+++ surveyValidation/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,request,session,render_template,flash
+from flask import Flask,redirect,request,render_template,flash
 
 
 app = Flask(__name__)
@@ -9,13 +9,20 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    if len(request.form['name']) < 1:
-        flash("name cannot be empty")
-    else:
-        flash("success your name is {}".format(request.form['name']))
     name = request.form['name']
     location = request.form['location']
     language = request.form['language']
     comment = request.form['comment']
-    return render_template('result.html',name=name,location=location,language=language,comment=comment)
+
+    if len(name) > 0 and len(comment) > 0 and len(comment) < 120:
+        return render_template('result.html',name=name,location=location,language=language,comment=comment)
+    else:
+        if len(name) == 0:
+            flash("Name cannot be empty")
+        if len(comment) == 0:
+            flash("Comment cannot be empty")
+        if len(comment) > 120:
+            flash("Comment cannot be more than 120 chars")
+    return redirect('/')
+
 app.run(debug=True)
