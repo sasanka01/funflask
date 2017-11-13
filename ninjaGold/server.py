2281a6a1694d6eab173243b5d8cc88da343bc786
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-12 17:39:45 -0800

    ninjagold

--- ninjaGold/server.py
+++ ninjaGold/server.py
@@ -12,27 +12,28 @@ def index():
     return render_template('index.html',gold=session['gold'],log=session['log'])
 
 
+
 @app.route('/process_money',methods=['POST'])
 def money():
     time = datetime.now()
     print time
     if request.form['building'] =='farm':
-        gold = random.randint(10,20)
-        session['log'].append({'state':"You entered a farm and won {} golds".format(gold), 'result':'win', 'time':time})
+        result = random.randint(10,20)
+
     elif request.form['building'] =='cave':
-        gold = random.randint(5,10)
-        session['log'].append({'state':"You entered a cave and won {} golds".format(gold), 'result':'win', 'time':time})
+        result = random.randint(5,10)
+
     elif request.form['building'] =='house':
-        gold = random.randint(2,5)
-        session['log'].append({'state':"You entered a house and won {} golds".format(gold), 'result':'win', 'time':time})
+        result = random.randint(2,5)
+
     elif request.form['building'] =='casino':
-        gold = random.randint(-50,50)
-        if gold < 0:
-            session['log'].append({'state':"You entered a casino and lost {} golds".format(gold), 'result':'loss', 'time':time})
-        else:
-            session['log'].append({'state':"You entered a casino and won {} golds".format(gold), 'result':'win', 'time':time})
-    session['gold'] += gold
-    print gold
+        result = random.randint(-50,50)
+
+    message = "you have won/lost {} Gold at {}".format(result,datetime.now().strftime("%H:%M"))
+    session['gold'] = int(session['gold']) + result
+    Log = session['log']
+    Log.append(message)
     return redirect('/')
+
 
 app.run(debug=True)
